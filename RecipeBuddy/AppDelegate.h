a7d65df5331d506ae4fdf5552cc537edec75508a
Sumedha <pramods@umich.edu>
2014-04-17 23:38:24 -0400

    Initial Commit

---
+++ RecipeBuddy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RecipeBuddy
+//
+//  Created by Sumedha Pramod on 4/17/14.
+//  Copyright (c) 2014 Sumedha Pramod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RecipeBuddy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RecipeBuddy
+//
+//  Created by Sumedha Pramod on 4/17/14.
+//  Copyright (c) 2014 Sumedha Pramod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
